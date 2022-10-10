5ee5384149430d73a26467950057b45f2e2a7fa5
Hemerson Violin <hemersonviolin@gmail.com>
2022-10-10 20:26:59 -0300

    Adicionado efeitos aleatórios do turtle

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-print("Hey")
+from turtle import *
+color('white', 'black')
+begin_fill()
+bgcolor("black")
+speed(1)
+setx(0)
+sety(0)
+
+while True:
+    forward(250)
+    left(91)
+    if abs(pos()) < 1:
+        break
+end_fill()
+
+done()
